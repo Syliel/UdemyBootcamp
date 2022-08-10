32da4065611c745825ec39d26f9b965819704f4b
Jennifer Gomez-Leon <jbellcollins@gmail.com>
2022-08-10 12:31:10 -0400

    reasons

--- Mongoose/MongooseExpress/index.js
+++ Mongoose/MongooseExpress/index.js
@@ -17,14 +17,22 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(methodOverride('_method'))
-app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', { products })
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category: category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
+
+
 });
 
 app.get('/products/new', (req, res) => {
@@ -36,25 +44,31 @@ app.post('/products', async (req, res) => {
     console.log(req.body)
     await newProduct.save();
     res.redirect(`/products/${newProduct._id}`)
-})
+});
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/show', { product })
-})
+});
 
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/edit', { product, categories })
-})
+});
 
 app.put('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
     res.redirect(`/products/${product._id}`)
-})
+});
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+});
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000")
